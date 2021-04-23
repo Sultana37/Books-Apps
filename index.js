979f1b5b05997b31a236e123d4c278ca2644a374
Esrat <imranzaman.318@gmail.com>
2021-04-23 10:11:54 -0400

    create Quotes div

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ return fetch('https://got-quotes.herokuapp.com/quotes')
         .then(res => res.json())
 }
 getqutoes().then(console.log)
+
+const createQutoesDiv =(qutoes) => {
+    const qutoescontainer = document.createElement('div')
+    const quote= document.createElement('p')
+    const character= document.createElement('p')
+    quote.innerText = qutoes.quote
+    character.innerText = qutoes.character
+
+    qutoescontainer.append(quote,character)
+    return qutoescontainer
+}
+
+getqutoes().then((quotes) => {
+ const quoteDiv = createQutoesDiv(quotes)
+console.log(quoteDiv)
+})
