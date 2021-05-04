0f6231a892421b1fa97f7e3d524b4f49db837c60
Esrat <imranzaman.318@gmail.com>
2021-05-04 00:45:24 -0400

    feature change  & add Style

--- index.js
+++ index.js
@@ -1,88 +1,123 @@
 const getqutoes= () =>{
-return fetch('https://got-quotes.herokuapp.com/quotes')
-        .then(res => res.json())
-}
-getqutoes().then(console.log)
-
-
-const changeRatingcolor = (quoteRating,ratinginput) => {
-
-    if (ratinginput.value >= 8) {
-     quoteRating.style.color ="green"
-    }
-    else if(ratinginput.value >= 4){
-        quoteRating.style.color = "blue"
-    }
-     else{
-    quoteRating.style.color ="red"
- }
+  return fetch('https://got-quotes.herokuapp.com/quotes')
+          .then(res => res.json())
+  }
+  getqutoes().then(console.log)
   
-}
+  
+  const changeRatingcolor = (quoteRating,ratinginput) => {
 
+      // //////////////////// ADDED //////////////////////////////////
+      try { 
+          if(ratinginput.value == "")  throw "empty";
+          if(isNaN(ratinginput.value)) throw "Not a number";
+          x = Number(ratinginput.value);
+          if(x < 0)  throw "Invalid rating number too low";
+          if (x > 10) throw " Invalid rating number too high";
 
-const createQutoesDiv =(qutoes) => {
-    const qutoescontainer = document.createElement('div')
-    const quotescontainertitle=document.createElement('h2')
-    const quote= document.createElement('p')
-    const character= document.createElement('p')
-    const quoteRating = document.createElement('p')
-    const ratinginput =document.createElement('input')
-    const ratingbtn = document.createElement('button')
-    //const submitbtn = document.getElementById('submit-btn');
-    //const sb = document.getElementById('search')
+        }
+        
+      
+        catch(err) {
+          alert(err);
+        }
+        //////////////////////  ///////////////////////////////////
+  
+        if (ratinginput.value >= 8) {
+          quoteRating.style.color ="green"
+         }
+         else if(ratinginput.value >= 4){
+             quoteRating.style.color = "blue"
+         }
+          else{
+         quoteRating.style.color ="black"
+      }
+    
+  }
+  
+  
+  const createQutoesDiv =(qutoes) => {
+      const qutoescontainer = document.createElement('div')
+      const quotescontainertitle=document.createElement('h2')
+      const quote= document.createElement('p')
+      const character= document.createElement('p')
+      const quoteRating = document.createElement('p')
+      const ratinginput =document.createElement('input')
+      const ratingbtn = document.createElement('button')
+      //const submitbtn = document.getElementById('submit-btn');
+      //const sb = document.getElementById('search')
+  
+  
+      quoteRating.id ="quote-rating"
+      ratinginput.id ="quote-rating-input"
+      quotescontainertitle.className="quote-container-title"
+  
+      quote.innerText = qutoes.quote
+      character.innerText = qutoes.character
+      quoteRating.innerText ='❣❣ Give Your Rating ❣❣'
+      ratingbtn.innerText ="Rate"
+      quotescontainertitle.innerText="Perfect quotes for you!"
+  
+      ratinginput.type = "number"
+      ratinginput.min = 1
+      ratinginput.max =10
+  
+      ratingbtn.addEventListener('click',() =>{
+      changeRatingcolor (quoteRating,ratinginput)
+         
+      })
+  
+     //submitbtn.addEventListener('submit',(e)=>{
+     // e.preventDefault();
+      //getcharacterqutoes(sb.value)
+     
+    // })
+  
+      qutoescontainer.append(quotescontainertitle,quote,character,quoteRating,ratinginput,ratingbtn)
+      return qutoescontainer
+  }
+  
+  const appendQuote = (quoteDiv) => {
+      const quotescontainer = document.getElementById('quote-container')
+      quotescontainer.innerHTML =""
+      quotescontainer.append(quoteDiv)
+     
+  }
 
-
-    quoteRating.id ="quote-rating"
-    ratinginput.id ="quote-rating-input"
-    quotescontainertitle.className="quote-container-title"
-
-    quote.innerText = qutoes.quote
-    character.innerText = qutoes.character
-    quoteRating.innerText ='Rating : '
-    ratingbtn.innerText ="Rate"
-    quotescontainertitle.innerText="Perfect quotes for you!"
-
-    ratinginput.type = "number"
-    ratinginput.min = 1
-    ratinginput.max =10
-
-    ratingbtn.addEventListener('click',() =>{
-    changeRatingcolor (quoteRating,ratinginput)
-       
+  const generatequtoe = () =>{
+    getqutoes().then((quotes) => {
+    const quoteDiv = createQutoesDiv(quotes)
+    appendQuote(quoteDiv)
     })
+    
+  }
+  const newQbutton = document.getElementById('n-q-btn')
+  
+  newQbutton.addEventListener('click' , generatequtoe)
+  
+  generatequtoe()
 
-   //submitbtn.addEventListener('submit',(e)=>{
-   // e.preventDefault();
-    //getcharacterqutoes(sb.value)
+    ////////////////// ADDED ///////////////////////////
+function searchChar (char) {
+
+    
+    return fetch('https://got-quotes.herokuapp.com/quotes?char=${char}')
+    .then(resp => resp.json())
+    .then(json =>{
    
-  // })
-
-    qutoescontainer.append(quotescontainertitle,quote,character,quoteRating,ratinginput,ratingbtn,sb,btn)
-    return qutoescontainer
-}
-
-const appendQuote = (quoteDiv) => {
-    const quotescontainer = document.getElementById('quote-container')
-    quotescontainer.innerHTML =""
-    quotescontainer.append(quoteDiv)
-   
-}
-const handleFetchError = ()=>{
-    const quotescontainer =document.getElementById('quote-container')
-    const warning =createElement('p')
-    warning.innerText="something went wrong "
-
-    quotescontainer.append(warning)
-}
-const generatequtoe = () =>{
-  getqutoes().then((quotes) => {
-  const quoteDiv = createQutoesDiv(quotes)
-  appendQuote(quoteDiv)
-  })
-  .catch(handleFetchError)
-}
-const newQbutton = document.getElementById('n-q-btn')
-
-newQbutton.addEventListener('click' , generatequtoe)
-
-generatequtoe()
+            let quot= document.createElement('p')
+            const charac= document.createElement('p')
+              quot = json.quote;
+            alert(json.quote)
+            document.getElementById('quote-container2').append(quot);
+    })
+    }
+    let val = document.createElement("INPUT");
+    let search = document.createElement("BUTTON");
+    search.innerHTML = "Search"
+    val.setAttribute("type", "search");
+    document.getElementById('quote-container2').appendChild(val);
+    document.getElementById('quote-container2').appendChild(search);
+    search.addEventListener('click',() =>{
+        searchChar(val.value)
+    })
